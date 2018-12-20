b6961a0563df306067f5af6bfeacc08bea808332
zhuxin <zhuxin@transfereasy.com>
2018-12-21 00:49:53 +0800

    blog-content

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,1 +1,110 @@
 # -*- coding:utf-8 -*-
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
+    return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/create', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'insert into post (title, body, author_id)'
+                'values (?, ? , ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'select p.id, title, body, created, author_id, username'
+        ' from post p join user u on p.author_id = u.id'
+        ' where p.id = ?', (id,)
+    ).fetchone()
+
+    """abort() 会引发一个特殊的异常，返回一个 HTTP 状态码。
+    它有一个可选参数， 用于显示出错信息，若不使用该参数则返回默认出错信息。 
+    404 表示“未找到”， 403 代表“禁止访问”。
+    （ 401 表示“未授权”，可以重定向到登录页面来代替返回这个状态码）
+    """
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    """check_author 参数的作用是函数可以用于在不检查作者的情况下获取一个post。
+    主要用于显示一个独立的帖子页面的情况，因为这时用户是谁没有关系，用户不会修改帖子。
+    """
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@bp.route('/<int:id>/update', methods=['GET', 'POST'])
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'update post set title = ?, body = ?'
+                ' where id = ?', (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=['GET', 'POST'])
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('delete from post where id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
