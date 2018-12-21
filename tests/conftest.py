2301a2bd58e8f0357128e333aeaf840f4cb8ef0c
zhuxin <zhuxin@transfereasy.com>
2018-12-21 23:50:47 +0800

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,78 @@
 # -*- coding:utf-8 -*-
+import os
+import tempfile
+
+import pytest
+from flaskr import create_app
+from flaskr.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+
+@pytest.fixture
+def app():
+    """ app test """
+
+    """tempfile.mkstemp() 创建并打开一个临时文件，返回该文件对象和路径。  
+    """
+    db_fd, db_path = tempfile.mkstemp()
+
+    """TESTING 告诉 Flask 应用处在测试模式下。 
+    Flask 会改变一些内部行为 以方便测试。其他的扩展也可以使用这个标志方便测试。
+    
+    DATABASE 路径被重载，这样它会指向临时路径，而不是实例文件夹。
+    设置好 路径之后，数据库表被创建，然后插入数据。测试结束后，临时文件会被关闭并 删除。
+    """
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    """ client test"""
+
+    """client 固件调用 app.test_client() 由 app 固件创建的应用 对象。
+    测试会使用客户端来向应用发送请求，而不用启动服务器。
+    """
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    """ runner test"""
+
+    """runner 固件类似于 client 。 
+    app.test_cli_runner() 创建一个运行器， 可以调用应用注册的 Click 命令。
+    """
+    return app.test_cli_runner()
+
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,1 +1,33 @@
 # -*- coding:utf-8 -*-
+import pytest
+from flask import g, session
+from flaskr.db import get_db
+
+
+def test_register(client, app):
+    assert client.get('/auth/register').status_code == 200
+    response = client.post(
+        '/auth/register',
+        data={'username': 'a', 'password': 'a'}
+    )
+    assert 'http://localhost/auth/login' == response.headers['Location']
+
+    with app.app_context():
+        assert get_db().execute(
+            "select * from user where username = 'a'",
+        ).fetchone() is not None
+
+
+# pytest.mark.parametrize 告诉 Pytest 以不同的参数运行同一个测试。
+# 这里用于测试不同的非法输入和出错信息，避免重复写三次相同的代码。
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+        ('', '', b'Username is required.'),
+        ('a', '', b'Password is required.'),
+        ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
